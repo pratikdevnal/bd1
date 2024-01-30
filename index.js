f5aba7e037c7fcd09fc1ba1b01c4340e5f169bcb
Pratik Devnal <79634014+pratikdevnal@users.noreply.github.com>
2024-01-31 02:19:56 +0530

    initial

---
+++ index.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("first", process.env.PORT);
+});
